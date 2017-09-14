bf9b0d0cfbbc21ab7861d33aea3f5a753aa3817f
Sam Lim <limsammy1@gmail.com>
2017-09-14 10:17:05 -0600

    Finished story, snack show page now shows location, average price, and snack count

--- spec/features/user_can_see_snack_page_spec.rb
+++ spec/features/user_can_see_snack_page_spec.rb
@@ -7,14 +7,24 @@ feature "When a user visits a snack page" do
     m_2 = sam.machines.create(location: "Turing Basement")
     s_1 = m_1.snacks.create(name: 'Cheetos', price: 2.50)
     s_2 = m_1.snacks.create(name: 'Doritos', price: 2.25)
+    s_3 = m_2.snacks.create(name: 'Lays', price: 2.15)
+    MachineSnack.create(snack_id: s_1.id, machine_id: m_2.id)
 
     visit machine_snack_path(m_1.id, s_1.id)
 
     expect(current_path).to eq("/machines/#{m_1.id}/snacks/#{s_1.id}")
     # I see the name of that snack
+    expect(page).to have_content(s_1.name)
     # I see the price for that snack
+    expect(page).to have_content(s_1.price)
     # I see a list of locations with vending machines that carry that snack
+    expect(page).to have_content(m_1.location)
+    expect(page).to have_content(m_2.location)
     # I see the average price for snacks in those vending machines
+    expect(page).to have_content("2.375")
+    expect(page).to have_content("2.325")
     # And I see a count of the different kinds of items in that vending machine.
+    expect(page).to have_content("Snack Count: #{m_1.snacks.count}")
+    expect(page).to have_content("Snack Count: #{m_2.snacks.count}")
   end
 end
