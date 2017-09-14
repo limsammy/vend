854334cfbaafa1fce03dd76b4c8a6a02e1560034
Sam Lim <limsammy1@gmail.com>
2017-09-14 09:11:54 -0600

    Added snack creation to spec

--- spec/features/user_sees_all_snacks_associated_with_single_vending_machine_spec.rb
+++ spec/features/user_sees_all_snacks_associated_with_single_vending_machine_spec.rb
@@ -4,9 +4,15 @@ feature "When a user visits the vending a single vending machine" do
   scenario "they see a list of vending machine snacks" do
     sam = Owner.create(name: "Sam's Snacks")
     v = sam.machines.create(location: "Turing Basement")
+    snack_1 = Snack.create(name: 'Cheetos', price: 2.50)
+    snack_2 = Snack.create(name: 'Doritos', price: 2.25)
 
-    visit machines_path(v)
+    visit machine_path(v)
 
-    expect(page).to have_content("Turing Basement")
+    expect(page).to have_content(snack_1.name)
+    expect(page).to have_content(snack_1.price)
+
+    expect(page).to have_content(snack_2.name)
+    expect(page).to have_content(snack_2.price)
   end
 end
