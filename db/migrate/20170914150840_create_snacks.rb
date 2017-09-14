0fa5b91a0054d334d834d4507f97912687354783
Sam Lim <limsammy1@gmail.com>
2017-09-14 09:09:23 -0600

    Created model and migration. Had to add FK

--- db/migrate/20170914150840_create_snacks.rb
+++ db/migrate/20170914150840_create_snacks.rb
@@ -3,6 +3,7 @@ class CreateSnacks < ActiveRecord::Migration[5.1]
     create_table :snacks do |t|
       t.string :name
       t.float :price
+      t.references :machine, foreign_key: true
 
       t.timestamps
     end
