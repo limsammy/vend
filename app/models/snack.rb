e5fbbde289e9ef4e1964c26c99484d2ec5f85ddd
Sam Lim <limsammy1@gmail.com>
2017-09-14 09:34:10 -0600

    User story 2 passing

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -1,3 +1,8 @@
 class Snack < ApplicationRecord
   belongs_to :machine
+
+  def self.average_price(machine_id)
+    machine = Machine.find(machine_id)
+    machine.snacks.average(:price)
+  end
 end
