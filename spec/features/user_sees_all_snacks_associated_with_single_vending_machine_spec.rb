45397732a948825062437f5818bf7ba3306c5895
Sam Lim <limsammy1@gmail.com>
2017-09-14 09:32:37 -0600

    user story 2 spec implemented and view changed. need to write class method

--- spec/features/user_sees_all_snacks_associated_with_single_vending_machine_spec.rb
+++ spec/features/user_sees_all_snacks_associated_with_single_vending_machine_spec.rb
@@ -15,4 +15,15 @@ feature "When a user visits the vending a single vending machine" do
     expect(page).to have_content(snack_2.name)
     expect(page).to have_content(snack_2.price)
   end
+
+  scenario "they see an average price of all snacks" do
+    sam = Owner.create(name: "Sam's Snacks")
+    machine = sam.machines.create(location: "Turing Basement")
+    snack_1 = machine.snacks.create(name: 'Cheetos', price: 2.50)
+    snack_2 = machine.snacks.create(name: 'Doritos', price: 2.25)
+
+    visit machine_path(machine)
+
+    expect(page).to have_content((snack_1.price + snack_2.price)/Snack.count)
+  end
 end
